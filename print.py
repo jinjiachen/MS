3982a9355c7afb19d06b91a95edc9d3b49f11b00
jinjiachen <jinjiachen_tux@163.com>
2014-10-10 16:30:20 +0800

    updated 20141010

---
+++ print.py
@@ -0,0 +1,14 @@
+#/usr/bin/python
+#coding=utf-8
+f=open('d:/repo/MS/print.txt','w')
+f.seek(50,0)
+f.write('公司名称:\n')
+f.write('库存:'+'\n')
+f.write('价格:'+'\n')
+f.write('数量:'+'\n')
+f.write('合计:'+'\n')
+f.write('总价:'+'\n')
+f.write('日期:'+'\n')
+f.write('开单人:'+'\n')
+f.write('收货方:'+'\n')
+
